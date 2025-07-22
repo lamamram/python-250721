d31576abcccb20a74c0807603724a136d658ef2f
mlamamra <mlamamra@dawan.fr>
2025-07-22 12:00:50 +0200

    types de donnees, sequences

--- matt_cours_types_sequences.py
+++ matt_cours_types_sequences.py
@@ -30,6 +30,7 @@ print(patient)
 # impossible de modifier un élément d'une str => immutabilité
 # first_name[0] = "P"
 # on est obligé de réaffecter la variable
+# REM: capitalize() retourne une valeur modifée MAIS ne modifie pas la variable
 first_name = first_name.capitalize()
 # %%
 patient = tuple(patient)
@@ -43,4 +44,61 @@ patient, type(patient), patient[0], len(patient)
 first_name, last_name, age, height = patient
 # first_name, last_name, age, height = patient[0], patient[1], patient[2], patient[3] 
 print(first_name, last_name, age, height)
+# %%
+# opérateur in
+phrase = "appeler un chat un chat"
+
+"chat" in phrase
+if "chat" not in phrase:
+  print("pas de chat dans la phrase")
+# %%
+# convertir une str en liste de mots
+mots = phrase.split()
+print(mots)
+print("chat" in mots)
+print(mots.count("chat"))
+# %%
+## mutabilité des listes
+# ERROR
+# mots[5] = "est"
+# append ne retourne pas une valeur mais modifie la variable
+print(mots.append("est"))
+mots.append("correct")
+print(mots)
+
+# %%
+# au lieu de faire 2x append on aurait pu ajouter une liste à 2 éléments à le 1ère
+# mots.extend(["correct", "correct"])
+# même chose
+mots + ["est", "correct"]
+
+# %%
+# supprimer
+# print(mots.pop(-1))
+# print(mots.pop(-1))
+# print(mots.pop())
+# print(mots.pop())
+print(mots)
+# %%
+# tranche entre l'indice de début compris
+# et l'indice de fin NON compris
+# mots[1:-2]
+mots[1:3]
+
+# tranche à partir de un et à la fin
+mots[1:]
+
+# tranche depuis le début jusqu'au 1er chat compris
+mots[:3]
+# %%
+# retourner l'index de 1ère occurence d'une valeur d'une liste
+mots.index("chat")
+mots[:mots.index("chat") + 1]
+# %%
+# joindre les éléments d'une liste de même type str
+phrase = " ".join(mots)
+# %%
+
+## in, slicing [:], .index(), .count(), fonctionne avec les str et tuples
+phrase[3:10], phrase.count("chat")
 # %%
