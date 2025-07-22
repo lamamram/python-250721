b8a23eb2df38ca8b4d349afd28887d02df89ceca
mlamamra <mlamamra@dawan.fr>
2025-07-22 10:09:30 +0200

    exo date: check avec if

--- matt_exo_date.py
+++ matt_exo_date.py
@@ -4,9 +4,18 @@
 # 3. décomposer ce nombre en nb en heure, minutes, secondes
 # 4. affichier le résulat <nb_hour>h <nb_min>m <nb_sec>
 
+import sys
+
 # number = int(input("saisir un nombre de secondes (0-86400) : "))
 number = input("saisir un nombre de secondes (0-86400) : ")
 # TODO vérifier que le nombre est bien convertible en entier
+
+# code avec arrêt
+if not number.isnumeric():
+  print("le nombre saisi n'est pas un entier valide")
+  # arrêter le programme
+  sys.exit()
+
 number = int(number)
 
 nb_hour = number // 3600
@@ -15,6 +24,20 @@ nb_minute = (number % 3600) // 60
 nb_second = number % 60
 
 print(nb_hour, "h", nb_minute, "m", nb_second, "s")
+
+# code avec if else
+# if number.isnumeric():
+#   number = int(number)
+
+#   nb_hour = number // 3600
+#   # utilisation des parenthèses pour forcer l'ordre des opérations
+#   nb_minute = (number % 3600) // 60
+#   nb_second = number % 60
+
+#   print(nb_hour, "h", nb_minute, "m", nb_second, "s")
+# else:
+#   print("le nombre saisi n'est pas un entier valide")
+
 # %%
 ## techniques de formatage de chaînes de caractères
 ## %%
