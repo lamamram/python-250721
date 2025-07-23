d5528ed88afd9b7e23dc3d85a39ecf7b236e36b2
mlamamra <mlamamra@dawan.fr>
2025-07-23 17:23:09 +0200

    fichiers

--- matt_cours_fichiers.py
+++ matt_cours_fichiers.py
@@ -16,3 +16,20 @@ with open("./users.csv", mode="r", encoding="utf-8") as f:
 print(lines)
 # f.read()
 # %%
+# écriture et en création ou en écrasement si le fichier existe
+with open("./clone_users.csv", mode="w", encoding="utf-8") as f:
+  # for line in lines:
+  #   f.write(line)
+  f.writelines(lines)
+
+
+# %%
+user = ["brenda", 28, "M", "admin"]
+# prétraitement du 28 en "28"
+for i in range(len(user)):
+  user[i] = str(user[i])
+
+# mode "a" comme "append" écriture à la fin du fichier
+with open("./users.csv", mode="a", encoding="utf-8") as f:
+  f.write(";".join(user))
+# %%
