ba2aeb6e1e666c3342207e57d6dd35b8f8c4352f
mlamamra <mlamamra@dawan.fr>
2025-07-23 16:32:02 +0200

    exception

--- matt_cours_exceptions.py
+++ matt_cours_exceptions.py
@@ -1,11 +1,20 @@
 # %%
+data = {
+  "name": "bob",
+  "age": 44,
+  "heigth": 1.70
+}
+
+data2 = {
+  "name": "bill",
+  "age": 43,
+  "heigth": 1.70,
+  "weight": 0
+}
+
+# %%
 ## bloc de code testé
 try:
-  data = {
-    "name": "bob",
-    "age": 44,
-    "heigth": 1.70
-  }
   # if "weight" in data:
   # Erreur ou Exception de type KeyError
   # le programme plante
@@ -15,4 +24,13 @@ try:
 except:
   print("erreur !!!")
 print("suite du programme")
+# %%
+## sécuriser une portion de code avec except Exception
+try:
+  data2["weight"]
+  ratio  = data2["heigth"]/data2["weight"]
+  print("end_try")
+# Exception est le type d'erreur générique qui capture tout
+except Exception as e:
+  print(e, type(e))
 # %%
