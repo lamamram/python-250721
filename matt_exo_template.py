6b255cde711c64df1fee2d4f99ffa758aa96b3e0
mlamamra <mlamamra@dawan.fr>
2025-07-23 10:29:57 +0200

    correction exo_template

--- matt_exo_template.py
+++ matt_exo_template.py
@@ -3,7 +3,7 @@ exercice : remplacer les clés entourées par "((" et "))"
 dans un texte par les valeurs correspondantes dans un dico
 
 1. afficher le contenu entre la première occurence de (( et ))
-2. remplacer ((key1)) par content1 dans _template
+2. remplacer ((pression)) par 500 dans _template
 Hint: regarder la fonction str.replace
 3. itérer sur _template pour remplacer toutes les slots (())
 par la clé correspondante si celle ci existe ou par N/A
@@ -26,5 +26,15 @@ injections = {
 
 # %%
 
-_template
+# while _template.find("((") != -1:
+while _template.count("(("):
+    start_index = _template.index("((")
+    end_index = _template.index("))")
+    key = _template[start_index + 2:end_index]
+    # gestion de clé manquante
+    val = injections.get(key, "N/A")
+
+    _template = _template.replace("((" + key + "))", val)
+
+print(_template)
 # %%
